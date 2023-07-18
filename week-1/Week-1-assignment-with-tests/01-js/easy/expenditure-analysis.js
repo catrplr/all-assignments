37cde8b9ba61becb6ae400afba059d8996638783
catrplr <deeproyrs@gmail.com>
2023-07-18 15:08:23 +0530

    new expenditure code

--- week-1/Week-1-assignment-with-tests/01-js/easy/expenditure-analysis.js
+++ week-1/Week-1-assignment-with-tests/01-js/easy/expenditure-analysis.js
@@ -8,8 +8,35 @@
   - `npm run test-expenditure-analysis`
 */
 
+function categorise(expendituref) {
+  var cat = [];
+  for (var i = 0; i < expendituref.length; i++) {
+      if (!cat.includes(expendituref[i].category)) {
+          cat.push(expendituref[i].category);
+      }
+  }
+  return cat;
+}
+
+
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+  itemcategory=categorise(transactions)
+    var Output=[]
+    for (var j=0;j<itemcategory.length;j++)
+    {
+        var FinalPrice=0;
+        for(var k=0;k<transactions.length;k++)
+        {
+            if (itemcategory[j]==transactions[k].category)
+            {
+                FinalPrice=FinalPrice+transactions[k].price
+
+            }
+        }
+        Output[itemcategory[j]]=FinalPrice ;   
+
+    }
+    console.log("Output:",Output);
 }
 
 module.exports = calculateTotalSpentByCategory;
